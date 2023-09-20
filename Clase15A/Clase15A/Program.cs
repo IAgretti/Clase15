fcc33982e3981645dac498bfb120d9e6f7fc44ea
Iri <89229099+IAgretti@users.noreply.github.com>
2023-09-20 09:53:34 -0300

    Ejercicio Clase Triángulo

---
+++ Clase15A/Clase15A/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace Clase15A
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+		
+			Triangulo t = new Triangulo();
+			t.Ingreso();
+			t.Imprimir();
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ Clase15A/Clase15A/Triangulo.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+namespace Clase15A
+{
+	/// <summary>
+	/// Description of Triangulo.
+	/// </summary>
+	public class Triangulo
+	{
+		public int l1, l2, l3;
+		
+		public void Ingreso(){
+			Console.Write("Ingrese el valor del lado 1: ");
+			l1 = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Ingrese el valor del lado 2: ");
+			l2 = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Ingrese el valor del lado 3: ");
+			l3 = Convert.ToInt32(Console.ReadLine());			              
+		}
+		public string TipoTriangulo(){
+			string tipo = "";
+			if ((l1 == l2) && (l1==l3))
+				tipo = "EQUILÁTERO";
+			else if ((l1 == l2) && (l1 != l3) || (l2 == l3) && (l3 != l1) || (l3 == l1) && (l1 != l2))
+				tipo = "ISÓSCELES";
+			else if ((l1 != l2) && (l1 != l3) && (l2 != l3))
+				tipo = "ESCALENO";
+			return tipo;
+		}
+		public void Imprimir(){
+			Console.WriteLine("El triángulo formado con los lados ingresados es: " + TipoTriangulo());
+		}
+	}
+}
